142350c5f56cf07786ed5f2f96d09d9bcff6d365
tblonski <tblonski@xogrp.com>
2017-08-08 12:40:08 -0400

    Add model validations.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     respond_to do |format|
-      if @post.save
+      if @post.valid? # or save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 describe Post do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:title) }
+  it { should validate_length_of(:title).is_at_least(5) }
 end

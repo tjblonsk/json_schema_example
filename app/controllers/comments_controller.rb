5a71ef849766fc8d012bce1a31344307c1b2f205
tblonski <tblonski@xogrp.com>
2017-08-09 12:31:12 -0400

    Clean up scaffolded controller code.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,84 +1,28 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
   before_action :validate_create_payload, only: [:create]
 
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
-
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comments/1
-  # DELETE /comments/1.json
-  def destroy
-    @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
+    if @comment.save
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def comment_params
-      params.require(:comment).permit(:name, :title, :length)
-    end
+  def comment_params
+    params.require(:comment).permit(:name, :title, :length)
+  end
 
-    def validate_create_payload
-      errors = JSON::Validator.fully_validate(Schemas::Comment.load, comment_params.to_h)
+  def validate_create_payload
+    errors = JSON::Validator.fully_validate(Schemas::Comment.load, comment_params.to_h)
 
-      unless errors.empty?
-        errors << 'Please visit http://docs.comments.apiary.io/ for a full API reference'
-        render json: { errors: errors }, status: :unprocessable_entity
-      end
+    unless errors.empty?
+      errors << 'Please visit http://docs.comments.apiary.io/ for a full API reference'
+      render json: { errors: errors }, status: :unprocessable_entity
     end
+  end
 end
